d74ea4826f17aaf12608e8c58dba455a90bcad75
zvonarevaav <zvonareva.a.v@yandex.ru>
2024-10-18 17:30:08 +0200

    исправление кода

--- wb_bot/bot/buttons.py
+++ wb_bot/bot/buttons.py
@@ -20,18 +20,16 @@ def create_time_unit_buttons():
     hour_button = InlineKeyboardButton(text="🕒 Часы", callback_data="h")
     day_button = InlineKeyboardButton(text="📅 Дни", callback_data="d")
     week_button = InlineKeyboardButton(text="📆 Недели", callback_data="w")
-    month_button = InlineKeyboardButton(text="🗓 Месяц", callback_data="mo")  # Кнопка "Месяц"
-    repeat_button = InlineKeyboardButton(text="🔄 Повторять",
-                                         callback_data="repeat")  # Кнопка для повторяющегося напоминания
+    month_button = InlineKeyboardButton(text="🗓 Месяц", callback_data="mo")
 
-    # Создаем inline-клавиатуру с кнопками
+    # Создаем inline-клавиатуру без кнопки "Повторять"
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [min_button, hour_button],  # первая строка с двумя кнопками
-        [day_button, week_button],  # вторая строка с двумя кнопками
-        [month_button],  # третья строка с кнопкой "Месяц"
-        [repeat_button]  # строка с кнопкой "Повторять"
+        [min_button, hour_button],  # первая строка
+        [day_button, week_button],  # вторая строка
+        [month_button]  # третья строка
     ])
     return keyboard
+
 
 
 
