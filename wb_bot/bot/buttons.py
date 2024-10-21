bde0c82ba8a57e655bf718e1fc3976d144c4be67
zvonarevaav <zvonareva.a.v@yandex.ru>
2024-10-21 14:37:25 +0200

    Внесены изменения для улучшения работы бота

--- wb_bot/bot/buttons.py
+++ wb_bot/bot/buttons.py
@@ -1,18 +1,12 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-
 # Кнопка для создания напоминания
 def create_reminder_button():
     reminder_button = InlineKeyboardButton(text="📝 Создать напоминание", callback_data="create_reminder")
-
-
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [reminder_button]
-        # Убираем строку с кнопкой для просмотра активных задач
-        # [active_tasks_button]
+        [reminder_button]  # Оставлена только кнопка для создания напоминания
     ])
     return keyboard
-
 
 # Кнопки для выбора единицы времени
 def create_time_unit_buttons():
@@ -22,14 +16,10 @@ def create_time_unit_buttons():
     week_button = InlineKeyboardButton(text="📆 Недели", callback_data="w")
     month_button = InlineKeyboardButton(text="🗓 Месяц", callback_data="mo")
 
-    # Создаем inline-клавиатуру без кнопки "Повторять"
+    # Удалены кнопки "Повторять" и "Удалить"
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [min_button, hour_button],  # первая строка
-        [day_button, week_button],  # вторая строка
-        [month_button]  # третья строка
+        [min_button, hour_button],  # Первая строка с двумя кнопками
+        [day_button, week_button],  # Вторая строка с двумя кнопками
+        [month_button]  # Третья строка с кнопкой "Месяц"
     ])
     return keyboard
-
-
-
-
